852845da3e4d4c359142b3ec8161c68c61e575c5
Samuel Carvalho <93744509+samueldecarvalhodev@users.noreply.github.com>
2022-11-16 19:53:56 -0300

    Index done

--- src/components/SmoothScrolling/Styled.ts
+++ src/components/SmoothScrolling/Styled.ts
@@ -6,7 +6,7 @@ const SmoothScrollingStyled = styled.a<SmoothScrollingStyledProps>`
   width: 6rem;
   height: 6rem;
   background-image: url(${(props) => props.backgroundImage.src});
-  bottom: 6rem;
+  bottom: 3rem;
   right: 6rem;
   border-radius: 50%;
   display: flex;
